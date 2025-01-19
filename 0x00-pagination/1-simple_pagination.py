4d5d0eca7e95167a90cbc666ffd08e15c39209aa
TLC <tituslifecharlie@gmail.com>
2025-01-20 00:50:46 +0100

    done

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,30 @@
+import csv
+from typing import List
+from .0_simple_helper_function import index_range
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Skip header
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page of the dataset."""
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+        
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        
+        return data[start:end] if start < len(data) else []
